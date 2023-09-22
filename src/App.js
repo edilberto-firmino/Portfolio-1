a344bad9a4b7a5b715fdcc12f0523a110705281e
edilberto-firmino <edilberto_ti@yahoo.com.br>
2023-09-22 20:53:02 -0300

    :bug: tentando parar  por hoje

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true); // Corrigido o nome da variável
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
